3cefcbcc254d64daf1ec540865a334577fbcce5e
Sha Jiang <shajiang@Shas-MacBook-Air.local>
2020-02-03 10:53:22 +0800

    fix

--- readtable.js
+++ readtable.js
@@ -3,11 +3,11 @@
 
 
 var rows=document.getElementsByTagName('table')[0].rows;
-undefined
+
 function getCellContent(c){
  return c.textContent;
 }
-undefined
+
 for(var i=0; i<rows.length; i++){
   console.log(getCellContent(rows[i].cells[1]));
 }
